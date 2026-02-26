77eb51db72abcd396a33f72bec0a64958ba69193
Darpan Saini <darpansaini19978@gmail.com>
2026-02-26 11:24:30 +0530

    error correction

--- factorial.py
+++ factorial.py
@@ -1,11 +1,9 @@
 def factorial(n):
     if n < 0:
-        return "Factorial not defined for negative numbers"
-    
+        raise ValueError("Negative numbers not allowed")
+
     result = 1
     for i in range(1, n + 1):
         result *= i
-    
+
     return result
-
-
